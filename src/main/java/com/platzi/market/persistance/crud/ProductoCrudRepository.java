3af4e5cdb695e083d52ab42572293dfa691e234d
Andy  Canales Roman <andy.canales@pyaing.local>
2023-07-04 17:51:06 -0500

    Repositorios

--- src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
@@ -3,6 +3,8 @@ package com.platzi.market.persistance.crud;
 import com.platzi.market.persistance.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+import java.util.List;
 
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    List<Producto> findByIdProducto(int idCategoria);
 }
