cee5a5d08506946e1f62022fd7c50d49e42fd33d
Andy  Canales Roman <andy.canales@pyaing.local>
2023-07-06 17:32:50 -0500

    Query Methods

--- src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistance/crud/ProductoCrudRepository.java
@@ -4,7 +4,9 @@ import com.platzi.market.persistance.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
-    List<Producto> findByIdProducto(int idCategoria);
+    List<Producto> findByIdProductoOrderByNombreAsc(int idCategoria);
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
